9142db5e4fa3e52eb34344f6307838b48a0a25d7
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2024-10-27 20:03:54 +0530

    Add project files.

---
+++ src/Modules/Events/Evently.Modules.Events.Api/Database/Migrations/20241027100023_AddInitialDb.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Evently.Modules.Events.Api.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "events");
+
+            migrationBuilder.CreateTable(
+                name: "events",
+                schema: "events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    starts_at_utc = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ends_at_utc = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_events", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "events",
+                schema: "events");
+        }
+    }
+}
